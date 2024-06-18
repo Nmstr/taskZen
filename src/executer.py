efe4af224147a5dcadfaf1f0c5f228dc20e2b126
Nmstr <jonaskleinen325@gmail.com>
2024-06-18 15:39:17 +0200

    added type annotations and docstrings

--- src/executer.py
+++ src/executer.py
@@ -3,23 +3,56 @@ import subprocess
 import time
 
 class Executer:
-    def __init__(self, ui, allKeys):
+    def __init__(self, ui, allKeys: dict) -> None:
+        """
+        Initialize the executer
+        
+        Parameters:
+            - ui (UInput): The UInput object
+            - allKeys (dict): The dictionary of all keys
+        """
         self.scriptData = None
         self.ui = ui
         self.allKeys = allKeys
 
-    def actionWait(self, sleepTime):
+    def actionWait(self, sleepTime: int) -> None:
+        """
+        Sleep for the specified amount of time
+        
+        Parameters:
+            - sleepTime (int): The amount of time to sleep in milliseconds
+        """
         time.sleep(sleepTime / 1000)
 
-    def actionPressKey(self, key):
+    def actionPressKey(self, key: str) -> None:
+        """
+        Press the specified key
+        
+        Parameters:
+            - key (str): The key to press
+        """
         self.ui.write(e.EV_KEY, self.allKeys.get(key), 1)
         self.ui.syn()
 
-    def actionReleaseKey(self, key):
+    def actionReleaseKey(self, key: str) -> None:
+        """
+        Release the specified key
+        
+        Parameters:
+            - key (str): The key to release
+        """
+        self.ui.write(e.EV_KEY, self.allKeys.get(key), 1)
         self.ui.write(e.EV_KEY, self.allKeys.get(key), 0)
         self.ui.syn()
 
-    def actionTapKey(self, key, modifier=None):
+    def actionTapKey(self, key: str, modifier: str = None) -> None:
+        """
+        Tap the specified key
+        
+        Parameters:
+            - key (str): The key to tap
+            - modifier (str, optional): The modifier to use. Defaults to None.
+        """
         if modifier == 'SHIFT':
             self.ui.write(e.EV_KEY, e.KEY_LEFTSHIFT, 1)
         self.ui.write(e.EV_KEY, self.allKeys.get(key), 1)
@@ -28,23 +61,50 @@ class Executer:
             self.ui.write(e.EV_KEY, e.KEY_LEFTSHIFT, 0)
         self.ui.syn()
     
-    def actionMoveAbsolute(self, x, y):
+    def actionMoveAbsolute(self, x: int, y: int) -> None:
+        """
+        Move the mouse to the specified coordinates
+        
+        Parameters:
+            - x (int): The x coordinate
+            - y (int): The y coordinate
+        """
         self.ui.write(e.EV_ABS, e.ABS_X, x)
         self.ui.write(e.EV_ABS, e.ABS_Y, y)
         self.ui.syn()
 
-    def actionMoveRelative(self, x, y):
+    def actionMoveRelative(self, x: int, y: int) -> None:
+        """
+        Move the mouse relative to the current position
+        
+        Parameters:
+            - x (int): The x coordinate
+            - y (int): The y coordinate
+        """
         self.ui.write(e.EV_REL, e.REL_X, x)
         self.ui.write(e.EV_REL, e.REL_Y, y)
         self.ui.syn()
     
-    def actionExec(self, command, blocking=False):
+    def actionExec(self, command: list, blocking: bool = False) -> None:
+        """
+        Execute the specified command
+        
+        Parameters:
+            - command (list): The command to execute
+            - blocking (bool, optional): Whether to block the thread. Defaults to False.
+        """
         if blocking:
             subprocess.call(command)
         else:
             subprocess.Popen(command)
 
-    def execute(self, scriptData):
+    def execute(self, scriptData: dict) -> None:
+        """
+        Execute the script
+        
+        Parameters:
+            - scriptData (dict): The script data
+        """
         self.scriptData = scriptData
         if not self.scriptData:
             print("Error: No script data")
