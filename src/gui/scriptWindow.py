743f8172666a2476bbbaca9cfbbdf2e806f36548
Nmstr <jonaskleinen325@gmail.com>
2024-06-30 22:42:53 +0200

    scripts can now be properly run

--- src/gui/scriptWindow.py
+++ src/gui/scriptWindow.py
@@ -2,6 +2,50 @@ from PySide6.QtWidgets import QMainWindow, QWidget, QFileDialog
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile
 import os
+import pty
+import subprocess
+
+from PySide6.QtCore import QObject, Signal, QThread
+
+class ScriptRunner(QObject):
+    finished = Signal()
+    error = Signal(Exception)
+    outputUpdated = Signal(str)
+
+    def __init__(self, filepath: str) -> None:
+        super().__init__()
+        self.filepath = filepath
+
+    def run(self):
+        try:
+            # The master_fd is the file descriptor of the master end of the pty
+            # The slave_fd is the file descriptor of the slave end of the pty
+            master_fd, slave_fd = pty.openpty()
+
+            # Start the subprocess with the slave end as its controlling terminal
+            process = subprocess.Popen(['taskZen', 'execute', '-fv', self.filepath],
+                                    stdout=slave_fd, stderr=subprocess.STDOUT, stdin=slave_fd, close_fds=True)
+
+            # Close the slave FD to avoid keeping it open unnecessarily
+            os.close(slave_fd)
+
+            while True:
+                # Read output from the master end of the pty
+                output = os.read(master_fd, 512)
+                if not output:
+                    break
+                #print(output.decode(), end='')
+                self.outputUpdated.emit(output.decode())
+
+            # Wait for the subprocess to finish
+            process.wait()
+
+        except Exception as e:
+            self.error.emit(e)
+        finally:
+            self.finished.emit()
+            # Make sure to close the master FD
+            os.close(master_fd)
 
 class ScriptWindow(QMainWindow):
     def __init__(self, parent: QWidget = None, filepath: str = None) -> None:
@@ -31,8 +75,8 @@ class ScriptWindow(QMainWindow):
                 self.ui.scriptInput.setPlainText(f.read())
 
     def saveScript(self) -> None:
-        outputText = 'Saving script...\n'
-        self.ui.sideText.setText(outputText)
+        self.outputText = 'Saving script...\n'
+        self.ui.sideText.setText(self.outputText)
         scriptData = self.ui.scriptInput.toPlainText()
 
         if self.filepath is None:
@@ -45,8 +89,31 @@ class ScriptWindow(QMainWindow):
         with open(self.filepath, 'w') as f:
             f.write(scriptData)
 
-        outputText = 'Script saved\n'
-        self.ui.sideText.setText(outputText)
+        self.outputText = 'Script saved\n'
+        self.ui.sideText.setText(self.outputText)
 
     def runScript(self) -> None:
-        print('run script')
+        self.outputText = 'Running script...\n'
+        self.ui.sideText.setText(self.outputText)
+
+        self.thread = QThread()
+        self.scriptRunner = ScriptRunner(self.filepath)
+        self.scriptRunner.moveToThread(self.thread)
+
+        # Connect signals
+        self.scriptRunner.finished.connect(self.thread.quit)
+        self.scriptRunner.finished.connect(self.scriptRunner.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.scriptRunner.outputUpdated.connect(self.updateSideText)
+
+        # Start the worker when the thread starts
+        self.thread.started.connect(self.scriptRunner.run)
+        self.thread.start()
+
+        self.ui.runBtn.setDisabled(True)
+        self.scriptRunner.finished.connect(lambda: self.ui.runBtn.setDisabled(False))
+
+    def updateSideText(self, text):
+        # Update the GUI safely with the new text
+        self.outputText += text
+        self.ui.sideText.setPlainText(self.outputText)
