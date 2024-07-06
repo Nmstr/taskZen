cb6f5ee4fd7a929cbae49a58863f22ae30bb8001
Nmstr <jonaskleinen325@gmail.com>
2024-07-06 19:31:11 +0200

    ui can now be refreshed + better looks

--- src/gui/homeWindow.py
+++ src/gui/homeWindow.py
@@ -12,6 +12,7 @@ import os
 class HomeWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setObjectName("HomeWindow")
         self.setWindowTitle("taskZen - Home")
 
         with open('src/gui/style.qss', 'r') as f:
@@ -30,6 +31,26 @@ class HomeWindow(QMainWindow):
         # Connect buttons
         self.ui.createScriptBtn.clicked.connect(lambda: ScriptWindow(self))
         self.ui.createDeviceBtn.clicked.connect(lambda: DeviceWindow(self))
+        self.ui.refreshBtn.clicked.connect(self.refreshEntries)
+
+        self.refreshEntries()
+
+    def refreshEntries(self) -> None:
+        layout = self.ui.scriptSideContent.layout()
+        while layout.count():
+            item = layout.takeAt(0)
+            widget = item.widget()
+            if widget is not None:
+                widget.setParent(None)
+            self.ui.scriptSideContent.setFixedHeight(self.ui.scriptSideContent.height() - 75)
+
+        layout = self.ui.deviceSideContent.layout()
+        while layout.count():
+            item = layout.takeAt(0)
+            widget = item.widget()
+            if widget is not None:
+                widget.setParent(None)
+            self.ui.deviceSideContent.setFixedHeight(self.ui.deviceSideContent.height() - 75)
 
         self.loadEntries('scripts', '~/.config/taskZen/scripts')
         self.loadEntries('devices', '~/.config/taskZen/devices')
@@ -38,14 +59,19 @@ class HomeWindow(QMainWindow):
         for filename in os.listdir(os.path.expanduser(directory)):
             if filename.endswith(".yaml"):
                 filepath = os.path.expanduser(f"{directory}/{filename}")
-                scriptData = yaml.safe_load(open(filepath))
-                scriptEntry = EntryWidget(self, target, scriptData['name'], filepath)
+                entryData = yaml.safe_load(open(filepath))
+                if type(entryData) == dict:
+                    entryName = entryData.get('name', 'Unnamed')
+                else:
+                    entryName = 'Unnamed'
+
+                scriptEntry = EntryWidget(self, target, entryName, filepath)
                 if target == 'scripts':
                     self.ui.scriptSideContent.layout().addWidget(scriptEntry)
-                    self.ui.scriptSideContent.setMinimumHeight(self.ui.scriptSideContent.height() + 75)
+                    self.ui.scriptSideContent.setFixedHeight(self.ui.scriptSideContent.height() + 75)
                 elif target == 'devices':
                     self.ui.deviceSideContent.layout().addWidget(scriptEntry)
-                    self.ui.deviceSideContent.setMinimumHeight(self.ui.deviceSideContent.height() + 75)
+                    self.ui.deviceSideContent.setFixedHeight(self.ui.deviceSideContent.height() + 75)
 
 if __name__ == "__main__":
     app = QApplication([])
